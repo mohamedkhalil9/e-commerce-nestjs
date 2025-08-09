096e5b1ebc88acf109330d74bda35cc79acab76f
Mohamed Khalil <mohamedkhalildev@gmail.com>
2025-08-09 17:22:39 +0300

    import app modules and use validation pipe

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { ProductModule } from './product/product.module';
+import { UserModule } from './user/user.module';
+import { ProfileModule } from './profile/profile.module';
+// import { MongooseModule } from '@nestjs/mongoose';
+import { PrismaModule } from './prisma/prisma.module';
+import { CategoryModule } from './category/category.module';
+
+@Module({
+  imports: [
+    // MongooseModule.forRoot(process.env.DB_URI ?? ''),
+    PrismaModule,
+    AuthModule,
+    UserModule,
+    ProfileModule,
+    ProductModule,
+    CategoryModule,
+  ],
+})
+export class AppModule {}
