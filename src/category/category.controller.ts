7e857a30d13591d5cc1ab87fb01fd61aef1e60c9
Mohamed Khalil <mohamedkhalildev@gmail.com>
2025-08-09 19:28:28 +0300

    feat: update and delete single category by id

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,23 +1,47 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { CategoryService } from './category.service';
-import { CreateCategoryDto } from './dto';
+import { CreateCategoryDto, UpdateCategoryDto } from './dto';
+import { Category } from '@prisma/client';
 
 @Controller('categories')
 export class CategoryController {
   constructor(private categoryService: CategoryService) { }
 
   @Post()
-  createCategory(@Body() createCategoryDto: CreateCategoryDto) {
+  createCategory(
+    @Body() createCategoryDto: CreateCategoryDto,
+  ): Promise<Category> {
     return this.categoryService.createCategory(createCategoryDto);
   }
 
   @Get()
-  getCategories() {
+  getCategories(): Promise<Category[]> {
     return this.categoryService.getCategories();
   }
 
   @Get(':id')
-  getCategory(@Param() param: { id: string }) {
-    return this.categoryService.getCategory(param.id);
+  getCategory(@Param() params: { id: string }): Promise<Category> {
+    return this.categoryService.getCategory(params.id);
+  }
+
+  @Patch(':id')
+  updateCategory(
+    @Param() params: { id: string },
+    @Body() updateCategoryDto: UpdateCategoryDto,
+  ): Promise<Category> {
+    return this.categoryService.updateCategory(params.id, updateCategoryDto);
+  }
+
+  @Delete(':id')
+  deleteCategory(@Param() params: { id: string }): Promise<string> {
+    return this.categoryService.deleteCategory(params.id);
   }
 }
--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,12 +1,19 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
+import { Category } from '@prisma/client';
 
 @Injectable()
 export class CategoryService {
   constructor(private prismaService: PrismaService) { }
 
-  async createCategory(createCategoryDto: CreateCategoryDto) {
+  async createCategory(
+    createCategoryDto: CreateCategoryDto,
+  ): Promise<Category> {
     try {
       const category = await this.prismaService.category.create({
         data: {
@@ -16,20 +23,74 @@ export class CategoryService {
       });
 
       return category;
-    } catch (error) {
-      throw new InternalServerErrorException('Error creating category', error);
+    } catch (err) {
+      throw new InternalServerErrorException(err.message);
     }
   }
 
-  async getCategories() {
-    const categories = await this.prismaService.category.findMany();
-    return categories;
+  async getCategories(): Promise<Category[]> {
+    try {
+      const categories = await this.prismaService.category.findMany();
+      return categories;
+    } catch (err) {
+      throw new InternalServerErrorException(err.message);
+    }
   }
 
-  async getCategory(id: string) {
-    const category = await this.prismaService.category.findUniqueOrThrow({
-      where: { id },
-    });
-    return category;
+  async getCategory(id: string): Promise<Category> {
+    try {
+      const category = await this.prismaService.category.findUnique({
+        where: { id },
+      });
+
+      if (!category)
+        throw new NotFoundException(`Category with id ${id} not found`);
+
+      return category;
+    } catch (err) {
+      throw new InternalServerErrorException(err.message);
+    }
+  }
+
+  async updateCategory(
+    id: string,
+    updateCategoryDto: CreateCategoryDto,
+  ): Promise<Category> {
+    try {
+      const category = await this.prismaService.category.findUnique({
+        where: { id },
+      });
+
+      if (!category)
+        throw new NotFoundException(`Category with id ${id} not found`);
+
+      const updatedCategory = await this.prismaService.category.update({
+        where: { id },
+        data: {
+          name: updateCategoryDto.name,
+          description: updateCategoryDto.description,
+        },
+      });
+
+      return updatedCategory;
+    } catch (err) {
+      throw new InternalServerErrorException(err.message);
+    }
+  }
+
+  async deleteCategory(id: string): Promise<string> {
+    try {
+      const category = await this.prismaService.category.findUnique({
+        where: { id },
+      });
+
+      if (!category)
+        throw new NotFoundException(`Category with id ${id} not found`);
+
+      await this.prismaService.category.delete({ where: { id } });
+      return `Category with id ${id} deleted successfully`;
+    } catch (err) {
+      throw new InternalServerErrorException(err.message);
+    }
   }
 }
