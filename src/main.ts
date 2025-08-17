fe1a8e3942184061337aa4b6d6b10c432e8bd2fe
Mohamed Khalil <mohamedkhalildev@gmail.com>
2025-08-17 15:27:09 +0300

    refactor: cors enabled

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.enableCors();
+  // app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
